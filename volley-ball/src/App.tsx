6347b0362eee57fc06dfb95d2908841d26b1ddf7
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-06-25 14:57:08 +0200

    solved VolleyBall algorithm

---
+++ volley-ball/src/App.tsx
@@ -0,0 +1,51 @@
+import { useRef, useState } from 'react';
+import './App.css';
+import volleyBall from './utils/volleyBall';
+
+
+
+function App() {
+  const prob1 = useRef<HTMLInputElement>(null);
+  const prob2 = useRef<HTMLInputElement>(null);
+
+  function startGame(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const probabiltyTeam1 = parseFloat(prob1.current!.value);
+    const probabiltyTeam2 = parseFloat(prob2.current!.value);
+    const gameResult = volleyBall(probabiltyTeam1, probabiltyTeam2)
+    setGameOutCome(gameResult);
+    console.log(gameResult)
+  }
+
+  const [gameOutCome, setGameOutCome] = useState<string[]>();
+
+  return (
+    <div className="App">
+      <h1>VolleyBall Game</h1>
+      <form className="App__form" onSubmit={startGame} >
+        <div className="App__form-elements">
+          <div className="App__individual-input" >
+            <label htmlFor="prob1"> Porbability team 1 wins</label>
+            <input ref={prob1} id="prob1" type="number" min="0" max="1" step=".05" />
+          </div>
+          <div className="App__individual-input">
+            <label htmlFor="prob2"> Porbability team 2 wins</label>
+            <input ref={prob2} id="prob2" type="number" min="0" max="1" step=".05" />
+          </div>
+        </div>
+        <button type="submit">Start Game</button>
+      </form>
+      <div className="App__display-game-outcome">
+        {gameOutCome &&
+          gameOutCome.map((set, index) => (
+            <div className="App__display-set" key={index} >
+              {set}
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ volley-ball/src/utils/volleyBall.ts
@@ -0,0 +1,68 @@
+export default function volleyBall(prob1: number, prob2: number) {
+  let listScores = ["0-0"], setStack: number[][] = [], currentSet = [0, 0], team1Serves = true;
+  while (!isVictory(setStack)) {
+    while (!isSetFinished(currentSet, setStack.length)) {
+      const pointTeam1 = currentSet[0];
+      const pointTeam2 = currentSet[1];
+      if (team1Serves) {
+        isPoint(prob1) ?
+          currentSet = [pointTeam1 + 1, pointTeam2]
+          :
+          currentSet = [pointTeam1, pointTeam2 + 1]
+      } else {
+        isPoint(prob2) ?
+          currentSet = [pointTeam1, pointTeam2 + 1]
+          :
+          currentSet = [pointTeam1 + 1, pointTeam2]
+      }
+      team1Serves = !team1Serves;
+      const setString = setStack.map(el => el.join("-"));
+      listScores.push([setString.join(" "), currentSet.join("-")].join(" "));
+    }
+    setStack.push(currentSet);
+    currentSet = [0, 0];
+  }
+  return listScores
+}
+
+function isVictory(setStack: Array<number[]>) {
+  if (setStack.length < 3) {
+    return false
+  } else if (setStack.length === 5) {
+    return true
+  }
+  const setCount = setStack.reduce((acc, current) => {
+
+    if (current[0] < current[1]) {
+      return acc - 1
+    }
+    return acc + 1
+  }, 0)
+  if (setStack.length === 3) {
+    return Math.abs(setCount) === 3 ? true : false
+  }
+  return Math.abs(setCount) === 2 ? true : false
+}
+
+function isSetFinished(currentPoint: Array<number>, numberOfSet: number) {
+
+  const scoreTeam1 = currentPoint[0], scoreTeam2 = currentPoint[1];
+  if (numberOfSet < 4) {
+
+    if (scoreTeam1 < 25 && scoreTeam2 < 25) {
+
+      return false;
+    }
+    return Math.abs(scoreTeam1 - scoreTeam2) >= 2 ? true : false
+  }
+
+  if (scoreTeam1 < 15 && scoreTeam2 < 15) {
+    return false
+  }
+  return Math.abs(scoreTeam1 - scoreTeam2) >= 2 ? true : false
+}
+
+function isPoint(prob: number) {
+  const probInfluence = Math.random();
+  return probInfluence > (1 - prob) ? true : false;
+}
