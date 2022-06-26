ba0943979127defb05181008093f31c7ec3c6648
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-06-26 20:41:21 +0200

    Updated coments

--- volley-ball/src/utils/volleyBall.ts
+++ volley-ball/src/utils/volleyBall.ts
@@ -1,15 +1,22 @@
+// Three variables are key to this program - isPoint, isSetFinished, isVictory
+// - isPoint captures if a team won a point
+// - isSetFinished checks to see if a set is complete
+// - isVictory checks to see if a team has sufficient sets to win a game
+// Whether or not a team wins the point depends on a random number and the input probability for the team (see lines 89 to 92)
+
+
 export default function volleyBall(prob1: number, prob2: number) {
   let listScores = ["0-0"], // Array of strings that will be displayed at the end of the game
     listOfSets: number[][] = [], // Collection of arrays that represent the points for each set that has ended
     currentSet = [0, 0], // Array that represent the score of the current set being played
     team1Serves = true; // Boolean to keep track of which team is serving
 
-  while (!isVictory(listOfSets)) {    // Loop is going to run for each set
+  while (!isVictory(listOfSets)) {    // Loop runs for each set
 
     team1Serves = listOfSets.length % 2 === 0 ? true : false;
     const setString = listOfSets.map((el) => el.join("-"));   // Used at the end of the while loop below
 
-    while (!isSetFinished(currentSet, listOfSets.length)) {   // Loop is going to run for each point 
+    while (!isSetFinished(currentSet, listOfSets.length)) {   // Loop runs for each point 
       const pointTeam1 = currentSet[0];
       const pointTeam2 = currentSet[1];
       if (team1Serves) {
@@ -29,10 +36,10 @@ export default function volleyBall(prob1: number, prob2: number) {
           team1Serves = true;
         }
       }
-      listScores.push([setString.join(" "), currentSet.join("-")].join(" "));   // Updating the score array after each point
+      listScores.push([setString.join(" "), currentSet.join("-")].join(" "));   // Updates the "score" array after each point
 
     }
-    listOfSets.push(currentSet);    // Updating 
+    listOfSets.push(currentSet);    // Updates the "listOfSets" array after each set
     currentSet = [0, 0];
   }
 
@@ -40,7 +47,7 @@ export default function volleyBall(prob1: number, prob2: number) {
 }
 
 
-export function isVictory(listOfSets: Array<number[]>) {    // Return a boolean representing the end of a game
+export function isVictory(listOfSets: Array<number[]>) {    // Returns a boolean representing the end of a game
   if (listOfSets.length < 3) {
     return false;
   } else if (listOfSets.length === 5) {
@@ -78,7 +85,7 @@ export function isSetFinished(    // Returns a boolean representing a set being 
   return Math.abs(scoreTeam1 - scoreTeam2) >= 2 ? true : false;
 }
 
-export function isPoint(prob: number) {   // Return a boolean representing the victory of a point
+export function isPoint(prob: number) {   // Returns a boolean representing the victory of a point
   const probInfluence = Math.random();
   return probInfluence > 1 - prob ? true : false;
 }
